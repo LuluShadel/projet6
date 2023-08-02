bdc7206d255081b9b319c02107ed51d0939aa1d7
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-02 16:40:54 +0200

    ajout de la fonction assync qui permet de retourner une erreur si problème, et d'envoyer et recevoir reponse en json. ajout de la fonction worklist qui recupère la div parent gallery et qui rajoute les balise img et figcaption en fonction du tableau dans la div figure. enfin appelle de la fonction worklist.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,30 +1,41 @@
-// creation de figure 
-const divGallery = document.querySelector(".gallery");
-const figure = document.createElement("figure");
-divGallery.appendChild(figure);
 
-//creation des balise img et figcaption 
+const url = "http://localhost:5678/api/works";
 
-const img = document.createElement("img");
-img.setAttribute("id","img");
-figure.appendChild(img)
+let work = [];
 
-const title = document.createElement("figcaption");
-title.setAttribute("id","title");
-figure.appendChild(title);
+// fonction assync avec await qui interrompt l'execution et attend la resolution de la promesse
+const request = async () => {
+    await fetch(url)
+    //pour analyse réponse en json et retourner les données en json
+    .then ((resolve)=>resolve.json())
+    // ligne du tableau = donnée
+    .then((data)=>{
+        work = data
+    })
+    // catch = plusieurs instruction à utiliser en cas d'erreur 
+    .catch(function(error){
+        alert('http error'+error.status)
+    })
+}
+
+const workList = async()=>{
+    await request()
+    //recupération de la div parent et utilisation de map pour recuperer les elements du tableau 
+    document.querySelector(".gallery").innerHTML=work.map(
+        //ajout du bloc avec les `` à rajouter pour chaques lignes
+        (liste) =>`
+        <figure>
+        <img src="${liste.imageUrl}" alt="${liste.title}"></img>
+        <figcaption>${liste.title}</figcaption>
+</figure>`
+// permet de concaténé tous les élements d'un tableau 
+    ).join("")
+}
+workList()
 
 
 
 
-// image depuis l'api 
-fetch("http://localhost:5678/api/works")
-.then(res => res.json())
-.then(data => img.src = data[0].imageUrl);
-
-//title depuis l'api 
-fetch("http://localhost:5678/api/works")
-.then(res => res.json())
-.then(data => title.innerText = data[0].title);
 
 
 
