17068849dc28d4698a8798703a593516aad81f55
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-04 16:37:31 +0200

    ajout de la fonction filtre et de l'appel de l'api avec fetch (chemin get categories)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,22 @@
+// affichage des images page d'acceuil
+const urlWork = "http://localhost:5678/api/works";
 
-const url = "http://localhost:5678/api/works";
+let work = []
 
-let work = [];
 
-// fonction assync avec await qui interrompt l'execution et attend la resolution de la promesse
-const request = async () => {
-    await fetch(url)
-    //pour analyse réponse en json et retourner les données en json
-    .then ((resolve)=>resolve.json())
-    // ligne du tableau = donnée
+const card = async () =>{
+
+    await fetch(urlWork)
+    .then((response)=>response.json())
+    //recuperation dans notre tableau du tableau provenant de l'api
     .then((data)=>{
         work = data
-    })
-    // catch = plusieurs instruction à utiliser en cas d'erreur 
-    .catch(function(error){
-        alert('http error'+error.status)
+        
     })
 }
 
-const workList = async()=>{
-    await request()
+const cardList = async()=>{
+    await card()
     //recupération de la div parent et utilisation de map pour recuperer les elements du tableau 
     document.querySelector(".gallery").innerHTML=work.map(
         //ajout du bloc avec les `` à rajouter pour chaques lignes
@@ -28,10 +25,49 @@ const workList = async()=>{
         <img src="${liste.imageUrl}" alt="${liste.title}"></img>
         <figcaption>${liste.title}</figcaption>
 </figure>`
-// permet de concaténé tous les élements d'un tableau 
+// permet de concaténé tous les élements d'un tableau et de ne pas faire apparaittre les virgules par defaut 
     ).join("")
 }
-workList()
+cardList()
+
+
+// creation des boutons filtres 
+
+const urlCategorie = "http://localhost:5678/api/categories";
+
+const filtre = async() =>{
+    await fetch (urlCategorie)
+    .then((response)=>response.json())
+    .then((data)=>{
+        work=data
+        
+    })
+}
+
+
+//fonction qui ajout les filtres
+
+let divFiltre = document.getElementById("filtres")
+
+const filtreList = async()=>{
+    await filtre ()
+    divFiltre.innerHTML=work.map(
+       (liste)=>
+    
+        `<button class="btn btnFiltre${liste.id}">${liste.name}</button>`
+    ).join("")
+    
+}
+
+filtreList()
+
+// changement de couleur pour le filtre selectionner 
+
+
+
+
+
+
 
 
 
