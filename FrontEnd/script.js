ae313f530476b8582d1b49bc16970c88fc700f9e
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-15 14:31:52 +0200

    ajout de la fonction afficher filtre et triage filtre qui met à jour le dom en fonction des clic des boutons filtre + ajout de la classe active sur le bouton cliquer

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -8,7 +8,7 @@ let gallery = document.querySelector(".gallery")
 const urlWorks ="http://localhost:5678/api/works"
 
 //variable contenant chemin vers api catégorie 
-let urlCategorie = "http://localhost:5678/api/categories"
+let urlCategorie ="http://localhost:5678/api/categories"
 
 
 
@@ -24,15 +24,14 @@ return await response.json();
 
 async function appelApiCategorie(){
 const response = await fetch(urlCategorie)
-return await response.json();
+return await response.json()
+
 }
 
-
+let works= []
 // affichage des images
 async function affichageImage (){
-   let works= await appelApiWorks()
-   
-    
+   works = await appelApiWorks()
     //boucle permettant d'afficher les images 
     for(let i=0; i < works.length; i++){
         const element = works[i]
@@ -43,6 +42,7 @@ async function affichageImage (){
         <figcaption>${element.title}</figcaption>
         `
         gallery.appendChild(allWorks)
+        
     }
 }
 
@@ -52,9 +52,6 @@ async function affichageImage (){
 async function afficherfiltre(){
     let categories = await appelApiCategorie()
     
-
-    let ligne1 = categories[1]
-    console.log(ligne1)
     
     //boucle for pour chaque catégorie
     for (let i = 0; i < categories.length; i++) {
@@ -77,30 +74,111 @@ async function afficherfiltre(){
     btnTous.addEventListener("click",()=>{
         // efface avant chaque affichage
         gallery.innerHTML = ""
+        clearFilters()
+            btnTous.classList.add("active")
         affichageImage()
+        
 
     })
 
+}
 
-    // filtrage 
-    function clearFilters() {
+// fonction qui va retirer la class active  
+function clearFilters() {
 
-        const filters = document.getElementById("filtres");
-        
-        const children = filters.children;
-        for(let i = 0; i < children.length; i++) {
-            const child = children[i];
-            child.classList.remove("active");
-        }
-    };
-
+    const filters = document.getElementById("filtres")
+    
+    const children = filters.children
+    for(let i = 0; i < children.length; i++) {
+        const child = children[i]
+        child.classList.remove("active")
+    }
 }
 
 affichageImage()
 afficherfiltre()
 
 
+async function TriageFiltre (){
+    let categories = await appelApiCategorie()
+    works = await appelApiWorks()
 
+    //recuperation du btn objet
+    let btn1 = document.querySelector(".btn1")
+
+        //ecoute du clic bouton objet
+        btn1.addEventListener("click",function (){
+            gallery.innerHTML = ""
+            clearFilters()
+            btn1.classList.add("active")
+            const filtreObjet= works.filter(function(work){
+            
+                return work.category.id ===1  
+            })
+            console.log(filtreObjet)
+            for(let i=0; i <filtreObjet.length ; i++){
+                const element = filtreObjet[i]
+             
+                let allWorks = document.createElement("figure")
+                allWorks.innerHTML=`
+                <img src="${element.imageUrl}" alt="${element.title}">
+                <figcaption>${element.title}</figcaption>
+                `
+                gallery.appendChild(allWorks)
+                
+            }
+        }) 
+        let btn2 = document.querySelector(".btn2")
+
+        //ecoute du clic bouton objet
+        btn2.addEventListener("click",function (){
+            gallery.innerHTML = ""
+            clearFilters()
+            btn2.classList.add("active")
+            const filtreAppartement= works.filter(function(work){
+            
+                return work.category.id ===2  
+            })
+            for(let i=0; i <filtreAppartement.length ; i++){
+                const element = filtreAppartement[i]
+             
+                let allWorks = document.createElement("figure")
+                allWorks.innerHTML=`
+                <img src="${element.imageUrl}" alt="${element.title}">
+                <figcaption>${element.title}</figcaption>
+                `
+                gallery.appendChild(allWorks)
+                
+            }
+        }) 
+        let btn3 = document.querySelector(".btn3")
+
+        //ecoute du clic bouton objet
+        btn3.addEventListener("click",function (){
+            gallery.innerHTML = ""
+            clearFilters()
+            btn3.classList.add("active")
+            const filtreHotel= works.filter(function(work){
+            
+                return work.category.id ===3  
+            })
+            for(let i=0; i <filtreHotel.length ; i++){
+                const element = filtreHotel[i]
+             
+                let allWorks = document.createElement("figure")
+                allWorks.innerHTML=`
+                <img src="${element.imageUrl}" alt="${element.title}">
+                <figcaption>${element.title}</figcaption>
+                `
+                gallery.appendChild(allWorks)
+                
+            }
+        }) 
+    }
+        
+    
+
+TriageFiltre()
 
 
 
