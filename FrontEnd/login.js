62d9bbdf97cfef55db526c2a230d4a284c70fe01
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-15 14:34:07 +0200

    first commit

---
+++ FrontEnd/login.js
@@ -0,0 +1,8 @@
+
+const userMail = document.getElementById("champsEmail");
+const password = document.getElementById("champsPassword").value;
+
+// au clic lance fonction 
+// fonction verifie si login = login 
+
+console.log(userMail)
