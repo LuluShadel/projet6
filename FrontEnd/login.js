d869884e7ca4f44a9e39a54413bb1405457e55c6
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-16 16:04:59 +0200

    const charge utile et changement de l'objet data

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,8 +1,47 @@
 
-const userMail = document.getElementById("champsEmail");
-const password = document.getElementById("champsPassword").value;
+const userMail = document.querySelector(".champsEmail");
+const password = document.getElementById("champsPassword");
+const form = document.getElementById("login")
 
-// au clic lance fonction 
-// fonction verifie si login = login 
+console.log(form)
 
-console.log(userMail)
+
+
+const data = {
+    email: userMail.value,
+    password: password.value,
+}
+
+console.log(data)
+
+
+    
+
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+    
+
+        const chargeUtile= JSON.stringify(data)
+        console.log(chargeUtile)
+        
+fetch("http://localhost:5678/api/users/login",{
+    method : 'post',
+    headers: {
+        'Content-Type': 'application/json'      
+      },
+      body:chargeUtile,
+})
+.then (response =>{
+    if (response.ok){
+        return response.json()
+    }
+    else{
+        throw new Error ("Erreur lors de la requête")
+    }
+})
+})
+
+
+
+
+
