4d6f42748b8302ab2658522712482f46bd09dcd8
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-08-17 14:33:19 +0200

    ajout de la fonction pour effacer le token une fois logout, ajout de la fonction pour affficher le mode edition

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,38 +1,25 @@
 
-// Evenement du bouton tous (par defaut)
-
-//div parent pour les works
 let gallery = document.querySelector(".gallery")
-
-// variable contenant chemin vers api works 
-const urlWorks ="http://localhost:5678/api/works"
-
-//variable contenant chemin vers api catégorie 
+const urlWorks ="http://localhost:5678/api/works" 
 let urlCategorie ="http://localhost:5678/api/categories"
-
-
 
 // fonction recuperant l'api works
 async function appelApiWorks(){
- 
-
-//reponse au format json = lisible 
-const response = await fetch(urlWorks)
-return await response.json();
+    const response = await fetch(urlWorks)
+    return await response.json();
 }
 
 
 async function appelApiCategorie(){
-const response = await fetch(urlCategorie)
-return await response.json()
-
+    const response = await fetch(urlCategorie)
+    return await response.json()
 }
 
 let works= []
+
 // affichage des images
 async function affichageImage (){
    works = await appelApiWorks()
-    //boucle permettant d'afficher les images 
     for(let i=0; i < works.length; i++){
         const element = works[i]
      
@@ -41,51 +28,41 @@ async function affichageImage (){
         <img src="${element.imageUrl}" alt="${element.title}">
         <figcaption>${element.title}</figcaption>
         `
-        gallery.appendChild(allWorks)
-        
+        gallery.appendChild(allWorks)     
     }
 }
 
 
 // creation des boutons filtres 
-
 async function afficherfiltre(){
     let categories = await appelApiCategorie()
-    
     
     //boucle for pour chaque catégorie
     for (let i = 0; i < categories.length; i++) {
         const element = categories[i];
-
-        // récuperation de la div parent 
-       let divFiltre = document.getElementById("filtres")
+        let divFiltre = document.getElementById("filtres")
         
         //creation du boutton 
         let allFiltre = document.createElement("button")
         allFiltre.classList=`btnFiltre btn btn${element.id}`
         allFiltre.innerText=`${element.name}`
-        
-        //rattachement aux parents 
-        divFiltre.appendChild(allFiltre)
-        
+ 
+        divFiltre.appendChild(allFiltre)   
     }
+
     // evenement au clic du bouton tous (par defaut)
     const btnTous = document.querySelector(".btnTous")
+
     btnTous.addEventListener("click",()=>{
-        // efface avant chaque affichage
         gallery.innerHTML = ""
         clearFilters()
-            btnTous.classList.add("active")
+        btnTous.classList.add("active")
         affichageImage()
-        
-
     })
-
 }
 
 // fonction qui va retirer la class active  
 function clearFilters() {
-
     const filters = document.getElementById("filtres")
     
     const children = filters.children
@@ -95,12 +72,11 @@ function clearFilters() {
     }
 }
 
-affichageImage()
-afficherfiltre()
+
 
 
 async function TriageFiltre (){
-    let categories = await appelApiCategorie()
+    
     works = await appelApiWorks()
 
     //recuperation du btn objet
@@ -109,13 +85,16 @@ async function TriageFiltre (){
         //ecoute du clic bouton objet
         btn1.addEventListener("click",function (){
             gallery.innerHTML = ""
+            //on efface la class active des bouton filtres
             clearFilters()
+            // on ajoute active sur le bouton cliquer
             btn1.classList.add("active")
+            //fonction qui filtre 
             const filtreObjet= works.filter(function(work){
-            
+            // retourne que si category.id=1 donc objet
                 return work.category.id ===1  
             })
-            console.log(filtreObjet)
+            // mise à jour du DOM en fonction du nouveau tableau filtrer
             for(let i=0; i <filtreObjet.length ; i++){
                 const element = filtreObjet[i]
              
@@ -135,6 +114,7 @@ async function TriageFiltre (){
             gallery.innerHTML = ""
             clearFilters()
             btn2.classList.add("active")
+            
             const filtreAppartement= works.filter(function(work){
             
                 return work.category.id ===2  
@@ -175,16 +155,58 @@ async function TriageFiltre (){
             }
         }) 
     }
+
         
     
+    affichageImage()
+    afficherfiltre()
+    TriageFiltre()
 
-TriageFiltre()
 
 
 
+    // mode edition 
 
+
+    const userToken = window.localStorage.getItem("userToken")
+
+const btnLogOut = document.querySelector(".btnLogOut")
+const btnLogin = document.querySelector(".btnLogin")
+const divEdition = document.querySelector(".edition")
+const divFiltre = document.getElementById("filtres")
+const modification = document.querySelector(".modification")
+
+
+    
+if (userToken !== null){
+    modeEditionLogin()
+
+    btnLogOut.addEventListener("click",function(){
+        modeEditionLogOut()
+    })
+}
+
+function modeEditionLogin(){
+    btnLogOut.classList.remove("inactive")
+    btnLogin.classList.add("inactive")
+    divEdition.classList.remove("inactive")
+    divFiltre.classList.add("inactive")
+    modification.classList.remove("inactive")
+
+}
+
+function modeEditionLogOut (){
+    window.localStorage.removeItem('userToken')
+        btnLogOut.classList.add("inactive")
+        btnLogin.classList.remove("inactive")
+        divEdition.classList.add("inactive")
+        divFiltre.classList.remove("inactive")
+        modification.classList.add("inactive")
+        
+}
 
    
+
 
 
 
